7f89f1f8d1283b161c916dbf18f5e49a17e274fe
Yijing Jiang <yijiang@lxplus083.cern.ch>
2016-07-30 14:40:55 +0200

    random data from a gaussian distribution with error bars

---
+++ gaus.C
@@ -0,0 +1,36 @@
+{
+int bins=50;
+        TF1 *g1 =new TF1("g1","gaus",-20,20);
+        g1->SetParameters(1,-2,2);
+        TH1 *h1=new TH1F("h1","gaus",bins,-20,20);
+        h1->Sumw2();
+        TH1 *h1_err=new TH1F("h1_err","gaus errors",bins,-20,20);
+        Double_t number;
+
+std::vector<Double_t> * volume=new std::vector<Double_t>;
+std::vector<Double_t> * frequency=new std::vector<Double_t>;
+std::vector<Double_t> * error=new std::vector<Double_t>;
+        for (int i=0;i<1000;i++)
+        {
+                number=g1->GetRandom(-20,20);         
+       h1->Fill(number);
+
+        }
+
+        for (int i = 0 ;i<bins;i++)
+{
+                number = h1 ->GetBinError(i+1);
+                
+ int element_volume=h1->GetBinContent(i+1);
+volume->push_back(element_volume);
+frequency->push_back(-20+i*40/bins);       
+error->push_back(number);       
+cout<<element_volume<<endl;                
+        }
+
+
+        new TCanvas;
+        h1->Draw("ep");
+              
+}
+
